a38009e3a17606c39ea117c18daca3bb922c7185
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-24 09:49:35 +0500

    40-SelectByClass is added

---
+++ 40-SelectByClass/main.js
@@ -0,0 +1,11 @@
+// todo: Its output is HTML collection, which like an array
+// const element = document.getElementsByClassName('cl')
+// for (let i = 0; i < element.length; i++) {
+//     console.log(element[i].innerHTML);
+// }
+
+//? If we want to change innerHTML. We observe that innerHTML of lists having class "cl" is changed to "<h1>Final List</h1>" 
+// const element = document.getElementsByClassName('cl')
+// for (let i = 0; i < element.length; i++) {    
+//     element[i].innerHTML = "<h1>Final List</h1>" 
+// }
