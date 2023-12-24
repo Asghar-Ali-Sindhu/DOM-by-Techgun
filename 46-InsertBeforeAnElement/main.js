32f11fc5e4cbd17f626855c86f22d8c9c1965f75
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-24 15:47:35 +0500

    InsertBeforeElement added

---
+++ 46-InsertBeforeAnElement/main.js
@@ -0,0 +1,34 @@
+// todo: Insert a newly created newList in the initial position
+// let element = document.getElementById("intro");
+// const position = element.firstElementChild
+// const newList = document.createElement("li")
+// newList.textContent = "New List Added"
+// element.insertBefore(newList, position)
+
+// ! Insert this created newList after First element
+// let element = document.getElementById("intro");
+// const position = element.firstElementChild.nextElementSibling
+// const newList = document.createElement("li")
+// newList.textContent = "New List Added"
+// element.insertBefore(newList, position)
+
+
+// * Insert this created newList after Second element
+// let element = document.getElementById("intro");
+// const position = element.firstElementChild.nextElementSibling.nextElementSibling
+// const newList = document.createElement("li")
+// newList.textContent = "New List Added"
+// element.insertBefore(newList, position)
+
+// todo Insert this created newList after Third element
+// let element = document.getElementById("intro");
+// const position = element.firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling
+// const newList = document.createElement("li")
+// newList.textContent = "New List Added"
+// element.insertBefore(newList, position)
+
+// ? Insert this newList before ul
+// let position = document.getElementById("intro");
+// const newList = document.createElement("li")
+// newList.textContent = "New List Added"
+// document.body.insertBefore(newList, position)
