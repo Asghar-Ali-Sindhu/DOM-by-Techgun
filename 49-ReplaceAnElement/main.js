3d92af4a585db3321c473ce450dbd471e790b3cc
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-24 16:30:59 +0500

    49-ReplaceElement

---
+++ 49-ReplaceAnElement/main.js
@@ -0,0 +1,21 @@
+// ! Replace the first element of ul with newly created list item "Humsafar"
+// const element = document.getElementById("intro")
+// const firstChild = element.firstElementChild
+// const toBeReplaced = document.createElement("li")
+// toBeReplaced.textContent ="Humsafar"
+// element.replaceChild(toBeReplaced, firstChild)
+
+// todo: Replace the second element of ul with the newly created list item "Humsafar"
+// const element = document.getElementById("intro")
+// const secondChild = element.firstElementChild.nextElementSibling
+// const toBeReplaced = document.createElement("li")
+// toBeReplaced.textContent ="Humsafar"
+// element.replaceChild(toBeReplaced, secondChild)
+
+
+// todo: Replace the second element of ul with the newly created list item "Humsafar"
+// const element = document.getElementById("intro")
+// const thirdChild = element.firstElementChild.nextElementSibling.nextElementSibling
+// const toBeReplaced = document.createElement("li")
+// toBeReplaced.textContent ="Humsafar"
+// element.replaceChild(toBeReplaced, thirdChild)
