f7653b7b634e2a7a0aedac1f822d9a3af6504d3c
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-24 13:06:17 +0500

    44-ChangeHTML is added

---
+++ 44-ChangeHTML/main.js
@@ -0,0 +1,8 @@
+//! Fecth the inner HTML
+// let element = document.getElementById("intro")
+// console.log(element.innerHTML);
+
+//? Change the innerHTML
+let element = document.getElementById("intro")
+element.innerHTML = "<h2> This is a heading 2</h2>"
+console.log(element.innerHTML);
