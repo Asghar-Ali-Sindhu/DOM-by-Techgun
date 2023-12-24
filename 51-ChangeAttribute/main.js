0085a3b32f65eafebf6f77b77f6d20a4ba121ba8
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-24 17:20:37 +0500

    51-ChangeAttribute is added

---
+++ 51-ChangeAttribute/main.js
@@ -0,0 +1,24 @@
+// todo: id and class name can be added directly using btn.id = "try" etc. However, to add other attributes, we use setAttribute method
+// const btn = document.getElementById("btn")
+// btn.setAttribute("name", "form1")
+// btn.className = "try"
+
+//? How to know already set attributes
+// const btn = document.getElementById("btn")
+// btn.setAttribute("name", "form1")
+// btn.className = "try pi"
+// // const val = btn.getAttribute("class")
+// const val = btn.getAttribute("name")
+// console.log(val);
+// // console.log(val);
+
+//! Remove already set attribute
+// const btn = document.getElementById("btn")
+// btn.removeAttribute("id")
+
+
+/ How to know already present attributes /
+// const btn = document.getElementById("btn")
+// // const hasAtt = btn.hasAttribute("class")
+// const hasAtt = btn.hasAttribute("name")
+// console.log(hasAtt);
