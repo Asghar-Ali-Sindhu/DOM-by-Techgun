b4688a7648a6b7346064a51028b2f62fc9338703
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-24 09:05:37 +0500

    38-DOM is added

---
+++ 38-DOM/main.js
@@ -0,0 +1,18 @@
+// todo: The Document Object Model (DOM) is an applicaion programming interface (API) for manipulating HTML and XML documents. (add, remove and modify parts of the document/HTML)
+
+//? With DOM, javascript get all the power it needs to create dynamic HTML
+//! i) It can change all the HTML elements in the page
+// ! ii) It can change all the attributes in the page
+//! iii) It can change all the CSS styles in the page
+//! iv) It can remove all the existing HTML elements and attributes.
+//! v) It can add new HTML elements and attributes
+//! vi) It can react to all existing HTML events in the page.
+//! vii) It can create new HTML events in the page.
+
+// todo: Important Note:
+//! In the DOM, all HTML elements are defined as objects. So they will have both property and method.
+//! DOM represents your web page.
+//! If you want to access any element in an HTML page, you always start with accessing the document object.
+//! The document is the root node or object.
+
+console.log("Hi");
