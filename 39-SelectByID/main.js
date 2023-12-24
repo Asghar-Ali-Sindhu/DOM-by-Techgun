cefed472fa685881fa1124f88a52dc6e67cd010b
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-24 09:32:49 +0500

    39-SelectById is added

---
+++ 39-SelectByID/main.js
@@ -0,0 +1,17 @@
+// todo: Select an element by using id
+// let element = document.getElementById("first")
+// console.log(element);
+
+//! If a particular id is not present in the document, it returns null. This means that an id "first-1" is not present in the document.
+// let element = document.getElementById("first-1")
+// console.log(element);
+
+//? Find the inner HTML of selected element
+// let element = document.getElementById("first");
+// console.log(element.innerHTML);
+
+// todo: set or change innerHTML of a selected element
+// let element = document.getElementById("first");
+// element.innerHTML = "<h1> New List 2</h1>"
+// console.log(element);
+// console.log(element.innerHTML);
