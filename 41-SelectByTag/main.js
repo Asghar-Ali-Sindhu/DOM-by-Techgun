c9ec4322cae65e3dd27f58ac16c0ce808c7d0cdc
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-24 11:35:08 +0500

    SelectByTag is added

---
+++ 41-SelectByTag/main.js
@@ -0,0 +1,26 @@
+// todo: Selection of elements using tag name, find innerHTML and change innerHTML
+// let element = document.getElementsByTagName('li')
+// console.log(element);
+// for (let i = 0; i < element.length; i++) {
+//     // console.log(element[i]);
+//     // console.log(element[i].innerHTML);
+//     element[i].innerHTML = "We are true Muslims"
+//     console.log(element[i].innerHTML);
+// }
+
+// ! Selelct all h2
+// let element = document.getElementsByTagName('h2')
+// for (let i = 0; i < element.length; i++) {
+//         // console.log(element[i].innerHTML);
+//         element[i].innerHTML = "Good Bye"
+//        console.log(element[i].innerHTML);
+// }
+
+/ Select only those h2 which lie inside a div /
+// const div1 = document.getElementById('div1')
+// console.log(div1);
+// const element = div1.getElementsByTagName('h2')
+// console.log(element);
+// for (let i = 0; i < element.length; i++) {
+//     console.log(element[i].innerHTML);    
+// }
