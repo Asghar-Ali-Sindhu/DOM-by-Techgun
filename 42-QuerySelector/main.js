81fdcec0cd8dd9a308789916d9b9025034d459dc
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-24 12:11:21 +0500

    42-QuerySelector is added

---
+++ 42-QuerySelector/main.js
@@ -0,0 +1,24 @@
+// todo: We select an element by CSS selector using querySelector.    We want to select those paragraphs which have id "intro".
+// let element = document.querySelector("p.intro")
+// element.innerHTML = "Great"
+// //! It will select only first paragraph satisfying the selector condition
+// console.log(element);
+
+
+// * It will select only first element satisfying the selector condition
+// let element = document.querySelector(".intro")
+// element.innerHTML = "Great"
+// console.log(element);
+
+/ To change all the elements satisfying the condition of the querySelector /
+// let element = document.querySelectorAll("p.intro")
+// for (let i = 0; i < element.length; i++) {
+//     console.log(element[i].innerHTML);
+// }
+
+//? In order to select all the element having class "intro"
+// const element = document.getElementsByClassName('intro')
+// for (let i = 0; i < element.length; i++) {
+//   console.log(  element[i].innerHTML);
+    
+// }
