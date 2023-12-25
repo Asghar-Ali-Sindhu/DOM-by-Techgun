a9d559c32d758db3bfad0287e3287886c50ccf34
Asghar Ali Sindhu <zainmathinter@gmail.com>
2023-12-25 10:22:55 +0500

    54-ChangeCSSClasses is added

---
+++ 54-ChangeCSSClasses/main.js
@@ -0,0 +1,44 @@
+
+// todo: If we want to replace a class.
+// const classes = document.getElementById('box')
+// console.log(classes.className);
+// classes.className = "color"
+// console.log(classes.className);
+
+// ! If we add another class
+// const classes = document.getElementById('box')
+// console.log(classes.className);
+// classes.className += " color"
+// console.log(classes.className);
+
+// ? How to know the already given classes
+// const cls = document.getElementById("box")
+// const allCls = cls.classList
+// for (let i = 0; i < allCls.length; i++) {
+//     console.log(allCls[i]);
+// }
+// for (let css of allCls) {
+//     console.log(css);
+// }
+
+// todo: Add a new class
+// const box = document.getElementById("box")
+// box.classList.add("try")
+
+// ! Remove a class
+// const box = document.getElementById("box")
+// box.classList.remove("dim")
+
+/ Replace a class /
+// const box = document.getElementById("box")
+// box.classList.replace("dim", "try")
+
+// todo: To know that either a particular class is there or not
+// const box = document.getElementById("box")
+// console.log(box.classList.contains("dim"));
+// console.log(box.classList.contains("color"));
+// console.log(box.classList.contains("try"));
+
+// ! Most Important is toggle.
+// const box = document.getElementById("box")
+// console.log(box.classList.toggle("dim"));
